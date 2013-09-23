3ee95aa05c9ea538b54e8d80612c50d4acbd7048
Michael Aufreiter <michael@substance.io>
2013-09-23 23:30:19 +0200

    Extract renderNode method.

--- article.js
+++ article.js
@@ -143,14 +143,19 @@ Article.Renderer.Prototype = function() {
 
     var docNodes = this.docCtrl.container.getTopLevelNodes();
     _.each(docNodes, function(n) {
-      var view = this.createView(n);
-      frag.appendChild(view.render().el);
-      // Lets you customize the resulting DOM sticking on the el element
-      // Example: Lens focus controls
-      if (this.options.afterRender) this.options.afterRender(this.docCtrl, view);
+      this.renderNode(n, frag);
     }, this);
     
     return frag;
+  };
+
+
+  this.renderNode = function(n, frag) {
+    var view = this.createView(n);
+    frag.appendChild(view.render().el);
+    // Lets you customize the resulting DOM sticking on the el element
+    // Example: Lens focus controls
+    if (this.options.afterRender) this.options.afterRender(this.docCtrl, view);
   };
 
 };
